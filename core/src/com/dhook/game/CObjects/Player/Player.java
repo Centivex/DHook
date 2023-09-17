6e272d4c8cbfc762cc6368946dd4e6822333b3c9
Centivex <abranhamscarnota@gmail.com>
2023-09-17 15:00:08 +0200

    hay movimeinto en todas direcciones, pero no se queda mirando hacia los laterales

--- core/src/com/dhook/game/CObjects/Player/Player.java
+++ core/src/com/dhook/game/CObjects/Player/Player.java
@@ -9,7 +9,6 @@ import com.dhook.game.CEngine.CObject;
 import com.dhook.game.CObjects.Player.Enum.UltimatePressAD;
 import com.dhook.game.CObjects.Player.Enum.UltimatePressWS;
 import com.dhook.game.General.Direction;
-import sun.tools.jconsole.JConsole;
 
 import java.util.ArrayList;
 
@@ -19,12 +18,18 @@ public class Player extends CObject {
     private ShapeRenderer circuloMovRender;
     private float velocityX = 100, velocityY = 100;
     private UltimatePressWS uPressWS;
+    private boolean pressW, pressS, pressA, pressD, movY, movX;
     private UltimatePressAD uPressAD;
 
+    private Direction directionPlayerX, directionPlayerY = Direction.UP;
     private Direction directionPlayer = Direction.UP;
 
     private float dashDuration = 0.2f; // Duración del dash en segundos
     private float dashTimer = 0;
+
+    private boolean test =false;
+    private float durationTest = 1f;
+    private float timerTest = 0;
 
 
     public Player() {
@@ -119,49 +124,116 @@ public class Player extends CObject {
 
         //moviemiento
         //mov horizontal-----------------------------
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.W)) {
             uPressWS = UltimatePressWS.W;
+            pressW = true;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.W) && uPressWS == UltimatePressWS.W) {
+            directionPlayerY = Direction.UP;
+            circleMov.y += velocityY * deltaTime;
+        } else if (!Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            pressW = false;
+            uPressWS = UltimatePressWS.S;
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
+            pressS = true;
             uPressWS = UltimatePressWS.S;
-        }
-
-
-        if (Gdx.input.isKeyPressed(Input.Keys.W) && uPressWS == UltimatePressWS.W) {
-            circleMov.y += velocityY * deltaTime;
-            directionPlayer = Direction.UP;
-        } else if (!Gdx.input.isKeyPressed(Input.Keys.W)) {
-            uPressWS = UltimatePressWS.S;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.S) && uPressWS == UltimatePressWS.S) {
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S) && uPressWS == UltimatePressWS.S) {
+            directionPlayerY = Direction.DOWN;
             circleMov.y -= velocityY * deltaTime;
         } else if (!Gdx.input.isKeyPressed(Input.Keys.S)) {
+            pressS = false;
             uPressWS = UltimatePressWS.W;
         }
 
+        if (pressW || pressS) {
+            movY = true;
+        } else {
+            movY = false;
+        }
+
+
         //mov vertical-------------------------------------------------
+
         if (Gdx.input.isKeyJustPressed(Input.Keys.A)) {
             uPressAD = UltimatePressAD.A;
+            pressA = true;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A) && uPressAD == UltimatePressAD.A) {
+            directionPlayerX = Direction.LEFT;
+            circleMov.x -= velocityX * deltaTime;
+        } else if (!Gdx.input.isKeyPressed(Input.Keys.A)) {
+            pressA = false;
+            uPressAD = UltimatePressAD.D;
         }
+
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.D)) {
             uPressAD = UltimatePressAD.D;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.A) && uPressAD == UltimatePressAD.A) {
-            circleMov.x -= velocityX * deltaTime;
-            ;
-        } else if (!Gdx.input.isKeyPressed(Input.Keys.A)) {
-            uPressAD = UltimatePressAD.D;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.D) && uPressAD == UltimatePressAD.D) {
+            pressD = true;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D) && uPressAD == UltimatePressAD.D) {
+            directionPlayerX = Direction.RIGHT;
             circleMov.x += velocityX * deltaTime;
         } else if (!Gdx.input.isKeyPressed(Input.Keys.D)) {
+            pressD = false;
             uPressAD = UltimatePressAD.A;
         }
+
+        if (pressA || pressD) {
+            movX = true;
+        } else {
+            movX = false;
+        }
+
+        //mov lateral----------------------------------------------------
+
+        if (movY == true && movX == false) {
+            directionPlayer = directionPlayerY;
+        } else if (movY == false && movX == true) {
+            directionPlayer = directionPlayerX;
+        } else if (movY == true && movX == true) {
+            if (directionPlayerY == Direction.UP && directionPlayerX == Direction.RIGHT) {
+                directionPlayer = Direction.UPRIGHT;
+            } else if (directionPlayerY == Direction.UP && directionPlayerX == Direction.LEFT) {
+                directionPlayer = Direction.UPLEFT;
+            } else if (directionPlayerY == Direction.DOWN && directionPlayerX == Direction.RIGHT) {
+                directionPlayer = Direction.DOWNRIGHT;
+            } else if (directionPlayerY == Direction.DOWN && directionPlayerX == Direction.LEFT) {
+                directionPlayer = Direction.DOWNLEFT;
+            }
+        }
+
+
+        //------------------------------------------------------------
+       if (Gdx.input.isKeyJustPressed(Input.Keys.T)) {
+            if (timerTest <= 0) {
+                timerTest = durationTest;
+            }
+            test = true;
+        }else {
+
+            if (timerTest > 0) {
+                timerTest -= Gdx.graphics.getDeltaTime();
+            }else if (timerTest <= 0) {
+                test = false;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println(directionPlayer);
+
+
     }
 
 
--- core/src/com/dhook/game/General/Direction.java
+++ core/src/com/dhook/game/General/Direction.java
@@ -1,5 +1,5 @@
 package com.dhook.game.General;
 
 public enum Direction {
-    UP, DOWN, RIGHT, LEFT, UPRIGHT, UPLEFT, DOWNUPRIGHT, DOWNLEFT
+    UP, DOWN, RIGHT, LEFT, UPRIGHT, UPLEFT, DOWNRIGHT, DOWNLEFT
 }
